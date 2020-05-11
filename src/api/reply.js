e3477875cdb9a5783c6cbb38ce40049c8441a0b5
hubqin <wuhanqinb@gmail.com>
2020-05-11 14:14:14 +0800

    replies list

---
+++ src/api/reply.js
@@ -0,0 +1,13 @@
+import { request, authRequest } from '@/utils/request'
+
+export function getReplies(topicId, data) {
+  return request('topics/' + topicId + '/replies', {
+    data: data
+  })
+}
+
+export function getUserReplies(userId, data) {
+  return request('users/' + userId + '/replies', {
+    data: data
+  })
+}
